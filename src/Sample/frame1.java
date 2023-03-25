68f5c61d7ca6fe4e45e9af7c740760fdfecb5732
Sayak Pan <sayakpan9635@gmail.com>
2023-03-25 12:36:59 +0530

    Modified

---
+++ src/Sample/frame1.java
@@ -0,0 +1,11 @@
+package Sample;
+
+import javax.swing.JFrame;
+
+public class frame1 {
+    static JFrame index = new JFrame();
+
+    public static void main(String[] args) {
+        index.setLayout(null);
+    }
+}
